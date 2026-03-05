f4ef5a9d6735a12a75601dbbfc55cc69e84ca0e9
Luis Guilherme Bezerra Gomes <luis.gomes@mv.com.br>
2026-03-05 09:44:55 -0300

    ajuste de config

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
-require("guilherme")
+local function safe_require(module)
+    local status, lib = pcall(require, module)
+    if not status then
+        print("Aguardando plugin: " .. module)
+        return nil
+    end
+    return lib
+end
+
+safe_require("guilherme")
