ad93942ed9041e6c0b887ef03396acb2f00676ef
luis.gomes <luis.gomes@mv.com.br>
2026-07-09 10:53:19 -0300

    fix(telescope): usa version='*' para garantir compatibilidade
    - Troca tag='0.1.8' por version='*' para sempre pegar versão estável
    - Resolve compatibilidade com Neovim v0.12.4
    - Corrige erro 'ft_to_lang' nil value

--- lua/guilherme/packer.lua
+++ lua/guilherme/packer.lua
@@ -25,7 +25,7 @@ return packer.startup(function(use)
 
     use({
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
+        version = "*",
         -- or                            , branch = '0.1.x',
         requires = { { "nvim-lua/plenary.nvim" } },
     })
